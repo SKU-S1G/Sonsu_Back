8025570e3fd6543756f23bd792878f20697caafa
1suk <jang1suk@sungkyul.ac.kr>
2025-04-04 15:31:11 +0900

    '제발'

---
+++ controller/Progress.js
@@ -0,0 +1,75 @@
+import pool from "../database.js";
+import { fetchProgressCategory } from "../utils/Progress.js";
+
+export const continueLesson = async (req, res) => {
+  const userId = req.user_id;
+  if (!userId) {
+    return res.status(400).json({ message: "사용자ID가 없습니다." });
+  }
+  try {
+    const [lastLesson] = await pool.query(
+      `SELECT * FROM lessons l JOIN user_lessons ul ON l.lesson_id = ul.lesson_id WHERE user_id = ?
+          ORDER BY ul.lesson_date DESC LIMIT 1 `,
+      [userId]
+    );
+
+    if (lastLesson.length === 0) {
+      return res.status(404).json({ message: "최근 강의가 없습니다" });
+      z;
+    }
+
+    const lastLessonId = lastLesson[0].lesson_id;
+    const nextLessonId = lastLessonId + 1;
+    const [nextLesson] = await pool.query(
+      `SELECT * FROM lessons 
+         WHERE lesson_id = ?`,
+      [nextLessonId]
+    );
+    if (nextLesson.length === 0) {
+      return res.status(404).json({ message: "다음 강의가 없습니다." });
+    }
+    res.status(200).json({ message: "최근 강의를 불러왔습니다.", nextLesson });
+  } catch (error) {
+    console.log("에러 발생:", error.message);
+    res.status(500).json({ message: "최근 강의 불러오는데 실패했습니다." });
+  }
+};
+
+export const progressTopic = async (req, res) => {
+  const userId = req.user_id;
+  if (!userId) {
+    return res.status(400).json({ message: "사용자 ID가 없습니다." });
+  }
+
+  try {
+    const [result] = await pool.query(
+      `SELECT l.lesson_id, ul.status
+          FROM lessons l
+          JOIN user_lessons ul ON l.lesson_id = ul.lesson_id
+          WHERE ul.user_id = ?`,
+      [userId]
+    );
+    res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "진행 상태를 가져오는 데 실패했습니다." });
+  }
+};
+
+export const progressCategory = async (req, res) => {
+  const userId = req.user_id;
+  if (!userId) {
+    return res.status(400).json({ message: "사용자 ID가 없습니다." });
+  }
+
+  try {
+    const completedCategories = await fetchProgressCategory(userId);
+    res.status(200).json(completedCategories);
+    // console.log(completedCategories);
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ message: "완료된 카테고리를 가져오는 데 실패했습니다." });
+  }
+};
---
+++ controller/Review.js
@@ -0,0 +1,53 @@
+import pool from "../database.js";
+
+export const lessonSaved = async (req, res) => {
+  const userId = req.user_id;
+  const lessonId = req.body.lessonId;
+
+  if (!userId || !lessonId) {
+    return res
+      .status(400)
+      .json({ message: "사용자 ID와 강의 ID가 필요합니다." });
+  }
+
+  try {
+    const [rows] = await pool.query(
+      `SELECT * FROM user_saved WHERE user_id = ? AND lesson_id = ?`,
+      [userId, lessonId]
+    );
+
+    if (rows.length > 0) {
+      return res
+        .status(200)
+        .json({ message: "이미 즐겨찾기에 추가된 강의입니다." });
+    }
+
+    await pool.query(
+      `INSERT INTO user_saved (user_id, lesson_id) VALUES (?, ?)`,
+      [userId, lessonId]
+    );
+    res.status(200).json({ message: "즐겨찾기 추가되었습니다." });
+    console.log(rows);
+  } catch (error) {
+    console.error("즐겨찾기 추가 실패:", error.message);
+    res.status(500).json({ message: "서버 오류가 발생했습니다." });
+  }
+};
+
+export const fetchSavedLesson = async (req, res) => {
+  const userId = req.user_id;
+
+  try {
+    const [rows] = await pool.query(
+      `SELECT us.userSaved_id, us.lesson_id, l.word, l.animation_path 
+         FROM user_saved us 
+         JOIN lessons l ON us.lesson_id = l.lesson_id 
+         WHERE us.user_id = ?`,
+      [userId]
+    );
+    res.status(200).json(rows);
+  } catch (error) {
+    console.log("불러오기 실패:", error);
+    res.status(500).json({ message: "서버 오류가 발생했습니다." });
+  }
+};
