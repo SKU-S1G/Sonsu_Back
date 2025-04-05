dd45d7267bcf86ffe2c591ba2e617dc10bc509fa
이호연 <ihoyeon@ihoyeon-ui-MacBookAir.local>
2025-04-05 16:04:46 +0900

    나 무서워

--- database.js
+++ database.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
-  password: "1sukadmin",
-  database: "db_test",
+  password: "",
+  port: 3306,
+  database: "Sonsu",
 });
 
 export default pool;
